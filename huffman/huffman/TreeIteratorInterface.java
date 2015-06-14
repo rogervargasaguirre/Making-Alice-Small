cb2cadf74e31674d1869c5b3ac9b0b697acaf918
rogervargasaguirre <rogervargasaguirre@gmail.com>
2015-06-13 22:26:57 -0700

    Update TreeIteratorInterface.java

--- huffman/huffman/TreeIteratorInterface.java
+++ huffman/huffman/TreeIteratorInterface.java
@@ -7,7 +7,17 @@
 package huffman;
 import java.util.*;
 /**
- * @author Carranno
+ *<pre>
+ *              Tree Iterator Interface
+ * File:        TreeIteratorInterface.java
+ * Description: Provide methods that a tree iterator must implemen
+ * @author      James Hua, Blake Hashimoto, Roger Vargas, Wei Jiang
+ * @since       6/1/15
+ * log:         6/1/15 Created the encode method
+ *              6/5/15 Created the decode method
+ * Special Thanks to
+ * Paul Bladek
+ * </pre>
  */
 public interface TreeIteratorInterface<T>
 {
